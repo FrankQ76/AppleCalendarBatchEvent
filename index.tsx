4d01225b3a411c6965c12b9edc7a937361377041
TattooedGeek <17057276+FrankQ76@users.noreply.github.com>
2025-12-04 00:11:20 -0500

    feat: Deploy app to static hosting
    Moves the React app and its dependencies to index.html to enable deployment on static hosting platforms like GitHub Pages, eliminating the need for a build step.

--- index.tsx
+++ index.tsx
@@ -1,15 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// This code has been moved to index.html to support GitHub Pages deployment without a build step.
